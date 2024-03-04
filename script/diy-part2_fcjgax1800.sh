77c82be6490643df6f7e478e97bffbaa13654ff0
vb1980 <vb1980@sina.com>
2024-03-05 00:02:10 +0800

    更新fcj config文件，增加主题；更新fcj自定义设置bash脚本，设置默认wifi名称和信道

--- script/diy-part2_fcjgax1800.sh
+++ script/diy-part2_fcjgax1800.sh
@@ -10,19 +10,19 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改openwrt登陆地址,把下面的192.168.123.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把ImmortalWrt修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/ImmortalWrt/G-AX1800/g' package/base-files/files/bin/config_generate
 
-# 修改开源驱动wifi名称
-#sed -i 's/OpenWrt/RAISECOM-MSG1500-$/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 # 修改闭源驱动2G wifi名称
-#sed -i 's/OpenWRT-2.4G/RAISECOM-MSG1500/g' package/kernel/mt-drivers/mt7615d/files/lib/wifi/mt_dbdc.sh
+sed -i 's/SSID1=CR660X_2.4G/SSID1=G-AX1800/g' package/emortal/mtkhqos_util/files/mt7615.1.2G.dat
 
 # 修改闭源驱动5G wifi名称
-#sed -i 's/OpenWRT-5G/RAISECOM-MSG1500-5G/g' package/kernel/mt-drivers/mt7615d/files/lib/wifi/mt_dbdc.sh
+sed -i 's/SSID1=Openwrt_5G/SSID1=G-AX1800-5G/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
+
+# 修改闭源驱动5G wifi信道
+sed -i 's/Channel=44/Channel=149/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
 
 # 添加个性信息
 #sed -i 's/22.5.5/22.5.5 by nanchuci/g' ./package/lean/default-settings/files/zzz-default-settings
