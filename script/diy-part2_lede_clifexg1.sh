e9ef75e3e4e88284e63c6158ee40755204980dbc
vb1980 <vb1980@sina.com>
2024-09-19 21:31:46 +0800

    add build C-Life XG1 from immortalwrt 18.04

--- script/diy-part2_lede_clifexg1.sh
+++ script/diy-part2_lede_clifexg1.sh
@@ -10,19 +10,19 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改openwrt登陆地址,把下面的192.168.123.1修改成你想要的就可以了
-sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把ImmortalWrt修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/C-Life_XG1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/C-Life_XG1/g' package/base-files/files/bin/config_generate
 
 # 修改闭源驱动2G wifi名称
-sed -i 's/SSID1=CR660X_2.4G/SSID1=CLife-XG1/g' package/emortal/mtkhqos_util/files/mt7615.1.2G.dat
+#sed -i 's/SSID1=CR660X_2.4G/SSID1=C-Life_XG1/g' package/emortal/mtkhqos_util/files/mt7615.1.2G.dat
 
 # 修改闭源驱动5G wifi名称
-sed -i 's/SSID1=Openwrt_5G/SSID1=CLife-XG1-5G/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
+#sed -i 's/SSID1=Openwrt_5G/SSID1=C-Life_XG1-5G/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
 
 # 修改闭源驱动5G wifi信道
-sed -i 's/Channel=44/Channel=149/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
+#sed -i 's/Channel=44/Channel=149/g' package/emortal/mtkhqos_util/files/mt7615.1.5G.dat
 
 # 添加个性信息
 #sed -i 's/22.5.5/22.5.5 by nanchuci/g' ./package/lean/default-settings/files/zzz-default-settings
@@ -34,7 +34,7 @@ sed -i 's/Channel=44/Channel=149/g' package/emortal/mtkhqos_util/files/mt7615.1.
 #rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
 
 # 更改时区
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
