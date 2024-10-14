6c62fee06f17dcc5601fccd3db9900e506e9a25c
xxs <39432982+xxs-2@users.noreply.github.com>
2024-10-14 02:18:02 +0000

    Update diy-part2_jcgq20.sh

--- script/diy-part2_jcgq20.sh
+++ script/diy-part2_jcgq20.sh
@@ -13,7 +13,7 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把ImmortalWrt修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/JCG-Q20/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/YANGGUANG/g' package/base-files/files/bin/config_generate
 
 # 修改闭源驱动2G wifi名称
 sed -i 's/SSID1=CR660X_2.4G/SSID1=school-2.4G/g' package/emortal/mtkhqos_util/files/mt7615.1.2G.dat
